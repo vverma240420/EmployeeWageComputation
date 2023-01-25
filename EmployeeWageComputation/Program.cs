62fe6ced7e990242947b3c7e59483e2b7ebc8d8c
Vivek Verma <vverma240420@gmail.com>
2023-01-25 19:48:38 +0530

    Calculate Daily Wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,21 +4,28 @@
     {
         static void Main(string[] args)
         {
-            int ispresent = 1;
+            const int FULL_TIME = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0, empWage = 0;
+
             Console.WriteLine("Welcome to EmployeeWage Computation");
 
             //UC1- EmployeeAttendence
             Random random = new Random();
             int empAttendence = random.Next(0, 2); //0 or 1
-            if(empAttendence == ispresent)
+            if(empAttendence == FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
 
                 Console.WriteLine("Employee is Absent");
+                //empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage:{0}", empWage);
             Console.ReadLine();
         }
     }
