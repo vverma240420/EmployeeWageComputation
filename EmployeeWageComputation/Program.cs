09defa07c6acb28e6e76a858e5f9d0eff2c65044
Vivek Verma <vverma240420@gmail.com>
2023-01-25 19:09:36 +0530

    Day 3 Problem

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
