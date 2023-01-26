08ab741e42e32e6c9473254a71136f3dedac09fd
Vivek Verma <vverma240420@gmail.com>
2023-01-26 10:04:13 +0530

    Employee part time wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,27 +5,33 @@
         static void Main(string[] args)
         {
             const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             int empHrs = 0, empWage = 0;
 
             Console.WriteLine("Welcome to EmployeeWage Computation");
 
-            //UC1- EmployeeAttendence
+            //UC3- PartTimeWage
             Random random = new Random();
-            int empAttendence = random.Next(0, 2); //0 or 1
+            int empAttendence = random.Next(0, 3); //0 or 1 or 2 it will generate
             if(empAttendence == FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Full time employee is Present");
                 empHrs = 8;
+            }
+            else if(empAttendence == PART_TIME)
+            {
+                Console.WriteLine("Part time employee is present");
+                empHrs = 4;
             }
             else
             {
-
                 Console.WriteLine("Employee is Absent");
                 //empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Employee Wage:{0}", empWage);
+            //Console.WriteLine("Employee wage:"+empWage);  old way
+            Console.WriteLine("Employee Wage:{0}", empWage);  //new way
             Console.ReadLine();
         }
     }
